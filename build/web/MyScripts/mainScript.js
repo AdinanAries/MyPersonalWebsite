585bbe485a54095458af8b429551b536d2c383ca
Mohammed Adinan <adinanaries@outlook.com>
2019-11-19 17:06:02 -0500

    building view layer of app

--- build/web/MyScripts/mainScript.js
+++ build/web/MyScripts/mainScript.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
     $("#ProjectsNavLink").click(function(event){
         document.getElementById("HomeNavLink").classList.remove("active");
         document.getElementById("BlogsNavLink").classList.remove("active");
+        document.getElementById("AboutNavLink").classList.remove("active");
         document.getElementById("ProjectsNavLink").classList.add("active");
         
         document.getElementById("MainHomePage").style.display = "none";
         document.getElementById("MainBlogsPage").style.display = "none";
+        document.getElementById("MainAboutPage").style.display = "none";
         document.getElementById("MainProjectsPage").style.display = "block";
     });
 });
@@ -14,10 +16,12 @@ $(document).ready(function(){
     $("#HomeNavLink").click(function(event){
         document.getElementById("ProjectsNavLink").classList.remove("active");
         document.getElementById("BlogsNavLink").classList.remove("active");
+        document.getElementById("AboutNavLink").classList.remove("active");
         document.getElementById("HomeNavLink").classList.add("active");
         
         document.getElementById("MainProjectsPage").style.display = "none";
         document.getElementById("MainBlogsPage").style.display = "none";
+        document.getElementById("MainAboutPage").style.display = "none";
         document.getElementById("MainHomePage").style.display = "block";
     });
 });
@@ -26,10 +30,26 @@ $(document).ready(function(){
     $("#BlogsNavLink").click(function(event){
         document.getElementById("ProjectsNavLink").classList.remove("active");
         document.getElementById("HomeNavLink").classList.remove("active");
+        document.getElementById("AboutNavLink").classList.remove("active");
         document.getElementById("BlogsNavLink").classList.add("active");
         
         document.getElementById("MainProjectsPage").style.display = "none";
         document.getElementById("MainHomePage").style.display = "none";
+        document.getElementById("MainAboutPage").style.display = "none";
         document.getElementById("MainBlogsPage").style.display = "block";
+    });
+});
+
+$(document).ready(function(){
+    $("#AboutNavLink").click(function(event){
+        document.getElementById("ProjectsNavLink").classList.remove("active");
+        document.getElementById("HomeNavLink").classList.remove("active");
+        document.getElementById("BlogsNavLink").classList.remove("active");
+        document.getElementById("AboutNavLink").classList.add("active");
+        
+        document.getElementById("MainProjectsPage").style.display = "none";
+        document.getElementById("MainHomePage").style.display = "none";
+        document.getElementById("MainBlogsPage").style.display = "none";
+        document.getElementById("MainAboutPage").style.display = "block";
     });
 });
