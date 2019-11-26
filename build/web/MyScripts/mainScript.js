96424dcbc1ad2fa5d4ef0dc3583ba63f5e83b78b
Mohammed Adinan <adinanaries@outlook.com>
2019-11-25 19:31:48 -0500

    working on features as being tested on real mobile phone browser

--- build/web/MyScripts/mainScript.js
+++ build/web/MyScripts/mainScript.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $("#ProjectsNavLink").click(function(event){
-        window.history.pushState("index.html", "Mohammed | AriesLab", "http://localhost/MyPortfolio/Projects");
         document.getElementById("HomeNavLink").classList.remove("active");
         document.getElementById("BlogsNavLink").classList.remove("active");
         document.getElementById("AboutNavLink").classList.remove("active");
@@ -10,12 +9,12 @@ $(document).ready(function(){
         document.getElementById("MainBlogsPage").style.display = "none";
         document.getElementById("MainAboutPage").style.display = "none";
         document.getElementById("MainProjectsPage").style.display = "block";
+        window.history.pushState("index.html", "Mohammed | AriesLab", "http://localhost/MyPortfolio/Projects");
     });
 });
 
 $(document).ready(function(){
     $("#HomeNavLink").click(function(event){
-        window.history.pushState("index.html", "Mohammed | AriesLab", "http://localhost/MyPortfolio/");
         document.getElementById("ProjectsNavLink").classList.remove("active");
         document.getElementById("BlogsNavLink").classList.remove("active");
         document.getElementById("AboutNavLink").classList.remove("active");
@@ -25,12 +24,12 @@ $(document).ready(function(){
         document.getElementById("MainBlogsPage").style.display = "none";
         document.getElementById("MainAboutPage").style.display = "none";
         document.getElementById("MainHomePage").style.display = "block";
+        window.history.pushState("index.html", "Mohammed | AriesLab", "http://localhost/MyPortfolio/");
     });
 });
 
 $(document).ready(function(){
     $("#BlogsNavLink").click(function(event){
-        window.history.pushState("index.html", "Mohammed | AriesLab", "http://localhost/MyPortfolio/Blogs");
         document.getElementById("ProjectsNavLink").classList.remove("active");
         document.getElementById("HomeNavLink").classList.remove("active");
         document.getElementById("AboutNavLink").classList.remove("active");
@@ -40,12 +39,12 @@ $(document).ready(function(){
         document.getElementById("MainHomePage").style.display = "none";
         document.getElementById("MainAboutPage").style.display = "none";
         document.getElementById("MainBlogsPage").style.display = "block";
+        window.history.pushState("index.html", "Mohammed | AriesLab", "http://localhost/MyPortfolio/Blogs");
     });
 });
 
 $(document).ready(function(){
     $("#AboutNavLink").click(function(event){
-        window.history.pushState("index.html", "Mohammed | AriesLab", "http://localhost/MyPortfolio/About");
         document.getElementById("ProjectsNavLink").classList.remove("active");
         document.getElementById("HomeNavLink").classList.remove("active");
         document.getElementById("BlogsNavLink").classList.remove("active");
@@ -55,5 +54,7 @@ $(document).ready(function(){
         document.getElementById("MainHomePage").style.display = "none";
         document.getElementById("MainBlogsPage").style.display = "none";
         document.getElementById("MainAboutPage").style.display = "block";
+        window.history.pushState("index.html", "Mohammed | AriesLab", "http://localhost/MyPortfolio/About");
     });
 });
+
